bbc0c9f478202acba6ab209c567d362552942b53
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2021-12-15 13:53:13 -0600

    trying alternative

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,13 +9,19 @@ chmod 757 /var/www/html/config
 sed -i 's/allow_url_include = Off/allow_url_include = On/' /etc/php/7.*/apache2/php.ini
 sed -i "s/$_DVWA[ \'recaptcha_public_key\' ]  = \'\';/$_DVWA[ \'recaptcha_public_key\' ]  = \'6LdBxpcdAAAAAAlfIS5z6QjgTV2e0e_xh-sEW4Ql\';/" /etc/php/7.*/apache2/php.ini
 sed -i "s/$_DVWA[ \'recaptcha_private_key\' ]  = \'\';/$_DVWA[ \'recaptcha_private_key\' ] = \'6LdBxpcdAAAAAGUoKS-UB5ec1t5HT0i19bra4sQw\';/" /etc/php/7.*/apache2/php.ini
-systemctl enable apache2 
-systemctl enable mysql
-systemctl start apache2 
-systemctl start mysql
+# systemctl enable apache2 
+# systemctl enable mysql
+# systemctl start apache2 
+# systemctl start mysql
+
+service apache2 start
+service mysql start
+
 mysql <<EOF
 create database dvwa;
 create user dvwa@localhost identified by 'p@ssw0rd';
 grant all on dvwa.* to dvwa@localhost;
 flush privileges;
 EOF
+
+wait -n 
