140127119733eed683bbc4e8fc145b1e931e4e58
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2021-12-15 11:42:04 -0600

    updated

--- start-docker.sh
+++ start-docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 CONTAINER="target-dvwa"
 sudo docker build -t $CONTAINER .
-sudo docker volumes create pcap
 sudo docker run -it --rm -v "$(pwd)"/volumes/pcap:/var/log/pcap -p 80:80 $(sudo docker images | grep $CONTAINER | awk '{print $3}')
