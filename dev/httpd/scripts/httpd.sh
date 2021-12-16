1ddd3a063d12eabb29c351e4e0c6eb44a7e77415
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2021-12-16 12:36:19 -0600

    trying alternative

--- dev/httpd/scripts/httpd.sh
+++ dev/httpd/scripts/httpd.sh
@@ -3,6 +3,6 @@ cd /usr/local/apache2/htdocs && cp config/config.inc.php.dist config/config.inc.
 chmod 757 /usr/local/apache2/htdocs/hackable/uploads/
 chmod 646 /usr/local/apache2/htdocs/external/phpids/0.6/lib/IDS/tmp/phpids_log.txt 
 chmod 757 /usr/local/apache2/htdocs/config
-sed -i 's/allow_url_include = Off/allow_url_include = On/' /etc/php/7.*/apache2/php.ini
-sed -i "s/$_DVWA[ \'recaptcha_public_key\' ]  = \'\';/$_DVWA[ \'recaptcha_public_key\' ]  = \'6LdBxpcdAAAAAAlfIS5z6QjgTV2e0e_xh-sEW4Ql\';/" /etc/php/7.*/apache2/php.ini
-sed -i "s/$_DVWA[ \'recaptcha_private_key\' ]  = \'\';/$_DVWA[ \'recaptcha_private_key\' ] = \'6LdBxpcdAAAAAGUoKS-UB5ec1t5HT0i19bra4sQw\';/" /etc/php/7.*/apache2/php.ini
+sed -i 's/allow_url_include = Off/allow_url_include = On/' /etc/php/7.4/apache2/php.ini
+sed -i "s/$_DVWA[ \'recaptcha_public_key\' ]  = \'\';/$_DVWA[ \'recaptcha_public_key\' ]  = \'6LdBxpcdAAAAAAlfIS5z6QjgTV2e0e_xh-sEW4Ql\';/" /etc/php/7.4/apache2/php.ini
+sed -i "s/$_DVWA[ \'recaptcha_private_key\' ]  = \'\';/$_DVWA[ \'recaptcha_private_key\' ] = \'6LdBxpcdAAAAAGUoKS-UB5ec1t5HT0i19bra4sQw\';/" /etc/php/7.4/apache2/php.ini
